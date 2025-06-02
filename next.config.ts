fa41d888f11d3593261daa9a5e874ae5516d8b80
makara roth <makararoth12@gmail.com>
2025-06-02 03:49:10 +0000

    I see this error with the app, reported by NextJS, please fix it. The error is reported as HTML but presented visually to the user).
    A > before the line number in the error source usually indicates the line of interest:
    
    > Unhandled Runtime Error: Error: Invalid src prop (https://variety.com/wp-content/uploads/2023/09/StrangerThings_StrangerThings4_9_02_14_28_08.jpg?w=1000&h=563&crop=1) on `next/image`, hostname "variety.com" is not configured under images in your `next.config.js`
    > See more info: https://nextjs.org/docs/messages/next-image-unconfigured-host. Error source: src/components/news/NewsCard.tsx (25:13) @ NewsCard
    >
    >   23 |         {article.urlToImage ? (
    >   24 |           <div className="relative h-48 w-full">
    > > 25 |             <Image
    >      |             ^
    >   26 |               src={article.urlToImage}
    >   27 |               alt={article.title}
    >   28 |               fill
    >
    > Call Stack
    > 12
    >
    > Show 6 ignore-listed frame(s)
    > Array.map
    > <anonymous> (0:0)
    > NewsCard
    > src/components/news/NewsCard.tsx (25:13)
    > <unknown>
    > src/components/news/NewsList.tsx (59:9)
    > Array.map
    > <anonymous> (0:0)
    > NewsList
    > src/components/news/NewsList.tsx (58:26)
    > HomePage
    > src/app/(main)/page.tsx (84:7)

--- next.config.ts
+++ next.config.ts
@@ -16,6 +16,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'variety.com',
+        port: '',
+        pathname: '/**',
+      }
     ],
   },
 };
