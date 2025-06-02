87eae7e869c3afd07cd827403661755863d95b1f
makara roth <makararoth12@gmail.com>
2025-06-02 06:24:58 +0000

    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6805990598879954"      crossorigin="anonymous"></script>
    pls fix it have this error message "Couldn't verify your site"

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -76,7 +76,7 @@ export default function RootLayout({
           async
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6805990598879954"
           crossOrigin="anonymous"
-          strategy="afterInteractive"
+          strategy="beforeInteractive"
         />
       </head>
       <body className="font-body antialiased min-h-screen flex flex-col">
